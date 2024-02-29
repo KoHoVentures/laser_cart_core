f8746d7fbf2f6b208be7770a4e9244a1e01091fa
Aadi0902 <aadiaadi12345@gmail.com>
2024-02-29 07:11:53 +0530

    add delimiter for serial data

--- sensors_pkg/scripts/optiflow_publisher.py
+++ sensors_pkg/scripts/optiflow_publisher.py
@@ -1,8 +1,9 @@
 import serial
 import struct
+import time
 
 # Open serial port
-ser = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=1)
+ser = serial.Serial('/dev/ttyAMA1', baudrate=115200, timeout=1)
 
 # Function to parse MultiWii Serial Protocol v2 messages
 def parse_msp_v2(data):
@@ -17,12 +18,11 @@ optical_flow_values = []
 try:
     while True:
         # Read line from serial port
-        line = ser.read_until()
-        
+        line = ser.read_until('$')
+        valueInstring = str(line)
+        time.sleep(1)
         print(line)
         # Parse MSP v2 message
         # data = parse_msp_v2(line)
-            
-    
 except KeyboardInterrupt:
     ser.close()
